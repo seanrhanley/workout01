27dabbf43aaca362e4b35d73dd712337a8af4103
Sean Hanley <sean.hanley@berkeley.edu>
2019-03-12 23:42:01 -0700

    Create make-shot-charts-script.R in code directory.

---
+++ code/make-shot-charts-script.R
@@ -0,0 +1,4 @@
+###Title: make-shot-charts-script.R
+###Description: Create graphic charts based on player shot data and output these graphics to the images directory.
+###Input(s): player shot data in csv form. No parameters or arguments passed to script.
+###Output(s): The script outputs graphics and charts based on player shot data to the images directory.
